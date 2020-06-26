1ad452a154dcc66354e6252a8879fc1dbcd24b87
stu204146-kiel <stu204146@mail.uni-kiel.de>
2020-06-26 15:27:01 +0200

    Add some more tests #5

--- src/test/BasicTests.hs
+++ src/test/BasicTests.hs
@@ -1,14 +1,32 @@
-module BasicTests ( basicTests ) where
+module BasicTests
+  ( basicTests
+  )
+where
 
-import Test.Hspec
+import           Test.Hspec                     ( Spec
+                                                , Expectation
+                                                , describe
+                                                , it
+                                                , shouldBe
+                                                )
 
-import Control.Monad ( void )
+import           Control.Monad                  ( void )
 
-import Language.Haskell.Exts
+import           Language.Haskell.Exts          ( Module
+                                                , ParseResult(..)
+                                                , parseModule
+                                                )
+import           Language.Haskell.Exts.Pretty   ( Pretty
+                                                , prettyPrint
+                                                )
 import           Test.HUnit.Base                ( assertFailure )
 
-import FreshVars
-import Application ( processModule, specialCons )
+import           FreshVars                      ( PMState(..)
+                                                , evalPM
+                                                )
+import           Application                    ( processModule
+                                                , specialCons
+                                                )
 
 basicTests :: Spec
 basicTests = describe "Basic unit tests" tests
@@ -33,27 +51,84 @@ tests = do
   it "should accept a simple function" $ do
     mod1 <- parseTestModule "module A where\nf :: a -> a\nf x = x"
     let mod2 = evalPM (processModule mod1) defaultState
-    if mod2 /= mod1 then assertFailure "Does not accept simple prog"
-    else return ()
+    mod2 `prettyShouldBe` mod1
   it "should transform pattern matching into case expressions" $ do
-    mod1 <- parseTestModule $ "module A where\nlengthL :: [a] -> Int\n"
-                            ++ "lengthL [] = 0\nlengthL(_:xs) = 1 + lengthL xs"
-    expected <- parseTestModule $ "module A where\nlengthL :: [a] -> Int\n"
-                                ++ "lengthL a0 = case a0 of\n"
-                                ++  "  []    -> 0\n"
-                                ++  "  a1:a2 -> 1 + lengthL a2"
+    mod1 <-
+      parseTestModule
+      $  "module A where\nlengthL :: [a] -> Int\n"
+      ++ "lengthL [] = 0\nlengthL(_:xs) = 1 + lengthL xs"
+    expected <-
+      parseTestModule
+      $  "module A where\nlengthL :: [a] -> Int\n"
+      ++ "lengthL a0 = case a0 of\n"
+      ++ "  []    -> 0\n"
+      ++ "  a1:a2 -> 1 + lengthL a2"
     let mod2 = evalPM (processModule mod1) defaultState
-    if expected /= mod2 then assertFailure "does not transform pm correctly"
-    else return ()
+    expected `prettyShouldBe` mod2
   it "should transform pattern matching in a partial function" $ do
-    mod1 <- parseTestModule $  "module A where\n"
-                            ++ "head :: [a] -> a\n"
-                            ++ "head (x:xs) = x"
+    mod1 <-
+      parseTestModule
+      $  "module A where\n"
+      ++ "head :: [a] -> a\n"
+      ++ "head (x:xs) = x"
     let mod2 = evalPM (processModule mod1) defaultState
-    expected <- parseTestModule $  "module A where\n"
-                                ++ "head :: [a] -> a\n"
-                                ++ "head a0 = case a0 of\n"
-                                ++ "  a1 : a2 -> a1\n"
-                                ++ "  [] -> undefined"
-    if expected /= mod2 then assertFailure "does not transform partial fun"
-    else return ()
+    expected <-
+      parseTestModule
+      $  "module A where\n"
+      ++ "head :: [a] -> a\n"
+      ++ "head a0 = case a0 of\n"
+      ++ "  a1 : a2 -> a1\n"
+      ++ "  [] -> undefined"
+    mod2 `prettyShouldBe` expected
+  it "should accept a simple guarded expression" $ do
+    mod1 <-
+      parseTestModule
+      $  "module A where\n"
+      ++ "id :: a -> a\n"
+      ++ "id x | otherwise = x"
+    let mod2 = evalPM (processModule mod1) defaultState
+    expected <-
+      parseTestModule
+      $  "module A where\n"
+      ++ "id :: a -> a\n"
+      ++ "id a0 = let a2 = undefined\n"
+      ++ "            a1 = case a0 of\n"
+      ++ "              a3 -> if otherwise then a3"
+      ++ "                    else a2\n"
+      ++ "         in a1"
+    mod2 `prettyShouldBe` expected
+  it "should accept a more complex guarded function" $ do
+    mod1 <-
+      parseTestModule
+      $  "module A where\n"
+      ++ "useless :: (a -> Bool) -> a -> a -> a\n"
+      ++ "useless p x y | p x       = x\n"
+      ++ "              | otherwise = y"
+    let mod2 = evalPM (processModule mod1) defaultState
+    expected <-
+      parseTestModule
+      $  "module A where\n"
+      ++ "useless :: (a -> Bool) -> a -> a -> a\n"
+      ++ "useless a0 a1 a2 = let\n"
+      ++ "                     a4 = undefined\n"
+      ++ "                     a3 = case a0 of\n"
+      ++ "                       a5 -> case a1 of\n"
+      ++ "                        a6 -> case a2 of\n"
+      ++ "                         a7 -> if a5 a6 then a6\n"
+      ++ "                               else if otherwise then a7\n"
+      ++ "                                    else a4\n"
+      ++ "                   in a3"
+    mod2 `prettyShouldBe` expected
+
+-------------------------------------------------------------------------------
+-- Pretty printing comparison                                                --
+-------------------------------------------------------------------------------
+
+-- | Pretty prints both values and tests whether the resulting strings are
+--   equal modulo whitespace.
+prettyShouldBe :: (Pretty a, Pretty b) => a -> b -> Expectation
+prettyShouldBe x y =
+  let discardWhitespace = unwords . words
+      prettyX           = discardWhitespace (prettyPrint x)
+      prettyY           = discardWhitespace (prettyPrint y)
+  in  prettyX `shouldBe` prettyY
