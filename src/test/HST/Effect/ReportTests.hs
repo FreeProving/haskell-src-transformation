072379cbf724b711b44f499e4df0609a81026e77
stu204146-kiel <stu204146@mail.uni-kiel.de>
2020-07-08 11:50:25 +0200

    Format the code #25

--- src/test/HST/Effect/ReportTests.hs
+++ src/test/HST/Effect/ReportTests.hs
@@ -1,22 +1,26 @@
-module HST.Effect.ReportTests ( testReportEffect ) where
+module HST.Effect.ReportTests
+  ( testReportEffect
+  )
+where
 
-import HST.Effect.Report ( Message(..), Report, Severity(..),  report, reportFatal, runReport )
-
-import           Polysemy                       ( Member
-                                                , Members
-                                                , Sem
-                                                , run
-                                                , runM
+import           HST.Effect.Report              ( Message(..)
+                                                , Report
+                                                , Severity(..)
+                                                , report
+                                                , reportFatal
+                                                , runReport
                                                 )
 
-import           Test.Hspec                     ( Expectation
-                                                , Spec
+import           Polysemy                       ( Member
+                                                , Sem
+                                                , run
+                                                )
+
+import           Test.Hspec                     ( Spec
                                                 , context
                                                 , describe
                                                 , it
                                                 , shouldBe
-                                                , shouldReturn
-                                                , shouldThrow
                                                 )
 
 
@@ -36,15 +40,15 @@ testRunReport = context "runReport" $ do
         comp :: Member Report r => Sem r Int
         comp = reportFatal msg >> return 42
     run (runReport comp) `shouldBe` ([msg], Nothing)
-  it "returns Just a value and a single message if a single message is reported" $ do
-    let msg = Message Warning "Some warning"
-        comp :: Member Report r => Sem r Int
-        comp = report msg >> return 42
-    run (runReport comp) `shouldBe` ([msg], Just 42)
+  it "returns Just a value and a single message if a single message is reported"
+    $ do
+        let msg = Message Warning "Some warning"
+            comp :: Member Report r => Sem r Int
+            comp = report msg >> return 42
+        run (runReport comp) `shouldBe` ([msg], Just 42)
   it "reports messages in the correct order" $ do
     let msg1 = Message Info "Some info"
         msg2 = Message Warning "Some warning"
         comp :: Member Report r => Sem r Int
         comp = report msg1 >> report msg2 >> return 42
     run (runReport comp) `shouldBe` ([msg1, msg2], Just 42)
-  
