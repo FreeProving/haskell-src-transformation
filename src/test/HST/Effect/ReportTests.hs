47a96899065d0cdea736bc115f2a7bb9985de992
stu204146-kiel <stu204146@mail.uni-kiel.de>
2020-07-08 15:14:24 +0200

    Add tests for reportToOutputOrCancel

--- src/test/HST/Effect/ReportTests.hs
+++ src/test/HST/Effect/ReportTests.hs
@@ -5,11 +5,13 @@ module HST.Effect.ReportTests
   )
 where
 
+import           HST.Effect.Cancel              ( runCancel )
 import           HST.Effect.Report              ( Message(..)
                                                 , Report
                                                 , Severity(..)
                                                 , report
                                                 , reportFatal
+                                                , reportToOutputOrCancel
                                                 , runReport
                                                 )
 
@@ -17,6 +19,7 @@ import           Polysemy                       ( Member
                                                 , Sem
                                                 , run
                                                 )
+import           Polysemy.Output                ( runOutputList )
 
 import           Test.Hspec                     ( Spec
                                                 , context
@@ -26,12 +29,13 @@ import           Test.Hspec                     ( Spec
                                                 )
 
 
--- | Test group for interpreters of the 'HST.Effect.Report.Report' effect
+-- | Test group for interpreters of the 'HST.Effect.Report.Report' effect.
 testReportEffect :: Spec
 testReportEffect = describe "HST.Effect.Report" $ do
   testRunReport
+  testReportToOutputOrCancel
 
--- | Test group for 'runReport' tests
+-- | Test group for 'runReport' tests.
 testRunReport :: Spec
 testRunReport = context "runReport" $ do
   it "returns Just a value and no messages if nothing is reported" $ do
@@ -55,3 +59,29 @@ testRunReport = context "runReport" $ do
         comp :: Member Report r => Sem r Int
         comp = report msg1 >> report msg2 >> return 42
     run (runReport comp) `shouldBe` ([msg1, msg2], Just 42)
+
+-- | Test group for 'reportToOutputOrCancel' tests.
+testReportToOutputOrCancel :: Spec
+testReportToOutputOrCancel = context "reportToOutputOrCancel" $ do
+  it "returns Just a value and no messages if nothing is reported" $ do
+    let comp :: Member Report r => Sem r Int
+        comp = return 42
+    outputRun comp `shouldBe` ([], Just 42)
+  it "returns Nothing and a single message if a fatal error is reported" $ do
+    let msg = Message Error "Some error"
+        comp :: Member Report r => Sem r Int
+        comp = reportFatal msg >> return 42
+    outputRun comp `shouldBe` ([msg], Nothing)
+  it "returns Just a value and a single message if a single message is reported"
+    $ do
+        let msg = Message Warning "Some warning"
+            comp :: Member Report r => Sem r Int
+            comp = report msg >> return 42
+        outputRun comp `shouldBe` ([msg], Just 42)
+  it "reports messages in the correct order" $ do
+    let msg1 = Message Info "Some info"
+        msg2 = Message Warning "Some warning"
+        comp :: Member Report r => Sem r Int
+        comp = report msg1 >> report msg2 >> return 42
+    outputRun comp `shouldBe` ([msg1, msg2], Just 42)
+  where outputRun = run . runOutputList . runCancel . reportToOutputOrCancel
