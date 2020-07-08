865459900c6dc9775152d819d9bc43ce7f19b340
stu204146-kiel <stu204146@mail.uni-kiel.de>
2020-07-08 11:30:03 +0200

    Correct ReportTest module name

--- src/test/HST/Effect/ReportTests.hs
+++ src/test/HST/Effect/ReportTests.hs
@@ -1,4 +1,6 @@
-module ReportTests ( testReportEffect ) where
+module HST.Effect.ReportTests ( testReportEffect ) where
+
+import HST.Effect.Report ( Message, Report, runReport )
 
 import           Polysemy                       ( Member
                                                 , Members
@@ -25,7 +27,7 @@ testReportEffect = describe "HST.Effect.Report" $ do
 
 testRunReport :: Spec
 testRunReport = context "runReport" $ do
-  it "returns a value and no messages if nothing is reported" $ do
+  it "returns Just a value and no messages if nothing is reported" $ do
     let comp :: Member Report r => Sem r Int
         comp = return 42
     run (runReport comp) `shouldBe` ([], Just 42)
