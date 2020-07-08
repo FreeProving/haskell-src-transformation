92c37e9c58cecb565d10d7827314044d3ca92426
stu204146-kiel <stu204146@mail.uni-kiel.de>
2020-07-08 16:11:31 +0200

    Add tests for filterReportedMessages #25

--- src/test/HST/Effect/ReportTests.hs
+++ src/test/HST/Effect/ReportTests.hs
@@ -9,6 +9,7 @@ import           HST.Effect.Cancel              ( runCancel )
 import           HST.Effect.Report              ( Message(..)
                                                 , Report
                                                 , Severity(..)
+                                                , filterReportedMessages
                                                 , report
                                                 , reportFatal
                                                 , reportToOutputOrCancel
@@ -34,6 +35,7 @@ testReportEffect :: Spec
 testReportEffect = describe "HST.Effect.Report" $ do
   testRunReport
   testReportToOutputOrCancel
+  testFilterReportedMessages
 
 -- | Test group for 'runReport' tests.
 testRunReport :: Spec
@@ -85,3 +87,45 @@ testReportToOutputOrCancel = context "reportToOutputOrCancel" $ do
         comp = report msg1 >> report msg2 >> return 42
     outputRun comp `shouldBe` ([msg1, msg2], Just 42)
   where outputRun = run . runOutputList . runCancel . reportToOutputOrCancel
+
+testFilterReportedMessages :: Spec
+testFilterReportedMessages = context "filterReportedMessages" $ do
+  it "should report nothing if predicate is always false" $ do
+    let msg1 = Message Info "Some info"
+        comp :: Member Report r => Sem r Int
+        comp = report msg1 >> return 42
+    runFilter (const False) comp `shouldBe` ([], Just 42)
+  it "should report no messages for tautologic predicate and no reports" $ do
+    let comp :: Member Report r => Sem r Int
+        comp = return 42
+    runFilter (const True) comp `shouldBe` ([], Just 42)
+  it "should report all messages for tautologic predicate" $ do
+    let msg = Message Warning "Some warning"
+        comp, comp2 :: Member Report r => Sem r Int
+        comp  = report msg >> return 42
+        comp2 = reportFatal msg >> return 42
+    runFilter (const True) comp `shouldBe` ([msg], Just 42)
+    runFilter (const True) comp2 `shouldBe` ([msg], Nothing)
+  it "should report all messages if all reported messages satisfy the predicate"
+    $ do
+        let msg1 = Message Warning "Some warning"
+            msg2 = Message Warning "Another warning"
+            comp :: Member Report r => Sem r Int
+            comp = report msg1 >> report msg2 >> return 42
+        runFilter isWarn comp `shouldBe` ([msg1, msg2], Just 42)
+  it "should report no messages if no reported message satisfies the predicate"
+    $ do
+        let msg1 = Message Info "Some info"
+            msg2 = Message Error "Some error"
+            comp :: Member Report r => Sem r Int
+            comp = report msg1 >> report msg2 >> return 42
+        runFilter isWarn comp `shouldBe` ([], Just 42)
+  it "should report all messages that satisfy the predicate" $ do
+    let msg1 = Message Info "Some info"
+        msg2 = Message Warning "Some warning"
+        comp :: Member Report r => Sem r Int
+        comp = report msg1 >> report msg2 >> return 42
+    runFilter isWarn comp `shouldBe` ([msg2], Just 42)
+ where
+  runFilter p = run . runReport . filterReportedMessages p
+  isWarn = (== Warning) . msgSeverity
