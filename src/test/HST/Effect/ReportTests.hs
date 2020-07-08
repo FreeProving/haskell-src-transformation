208453d52097819c5ecb75d50cbc604d6764926a
stu204146-kiel <stu204146@mail.uni-kiel.de>
2020-07-08 11:45:52 +0200

    Add some more tests to ReportTests #25

--- src/test/HST/Effect/ReportTests.hs
+++ src/test/HST/Effect/ReportTests.hs
@@ -1,6 +1,6 @@
 module HST.Effect.ReportTests ( testReportEffect ) where
 
-import HST.Effect.Report ( Message, Report, runReport )
+import HST.Effect.Report ( Message(..), Report, Severity(..),  report, reportFatal, runReport )
 
 import           Polysemy                       ( Member
                                                 , Members
@@ -31,3 +31,20 @@ testRunReport = context "runReport" $ do
     let comp :: Member Report r => Sem r Int
         comp = return 42
     run (runReport comp) `shouldBe` ([], Just 42)
+  it "returns Nothing and a single Message if a fatal error is reported" $ do
+    let msg = Message Error "Some error"
+        comp :: Member Report r => Sem r Int
+        comp = reportFatal msg >> return 42
+    run (runReport comp) `shouldBe` ([msg], Nothing)
+  it "returns Just a value and a single message if a single message is reported" $ do
+    let msg = Message Warning "Some warning"
+        comp :: Member Report r => Sem r Int
+        comp = report msg >> return 42
+    run (runReport comp) `shouldBe` ([msg], Just 42)
+  it "reports messages in the correct order" $ do
+    let msg1 = Message Info "Some info"
+        msg2 = Message Warning "Some warning"
+        comp :: Member Report r => Sem r Int
+        comp = report msg1 >> report msg2 >> return 42
+    run (runReport comp) `shouldBe` ([msg1, msg2], Just 42)
+  
