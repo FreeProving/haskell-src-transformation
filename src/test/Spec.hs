8109e6b6dbde4bb3bad87bb7c99ca9dcb36d248b
stu204146-kiel <stu204146@mail.uni-kiel.de>
2020-07-08 11:30:34 +0200

    Add ReportTests to Spec file #25

--- src/test/Spec.hs
+++ src/test/Spec.hs
@@ -4,8 +4,10 @@ import           Test.Hspec                     ( hspec )
 
 import           HST.CoreAlgorithmTests         ( testCoreAlgorithm )
 import           HST.Effect.CancelTests         ( testCancelEffect )
+import           HST.Effect.ReportTests         ( testReportEffect )
 
 main :: IO ()
 main = hspec $ do
   testCoreAlgorithm
   testCancelEffect
+  testReportEffect
