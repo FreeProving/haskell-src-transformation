0b0b375e9586ce9aff87b138a32d26b849409466
Justin Andresen <jan.dresen.95@gmail.com>
2020-07-11 20:42:31 +0200

    Add constructor entries to `Environment` #31

--- src/lib/HST/Environment.hs
+++ src/lib/HST/Environment.hs
@@ -2,13 +2,53 @@
 --   compiler's state.
 
 module HST.Environment
-  ( Environment
+  ( -- * Environment Entries
+    ConEntry(..)
+    -- * Environment
+  , Environment
   , emptyEnv
   )
 where
 
+import           Data.Map                       ( Map )
+import qualified Data.Map                      as Map
+import qualified Language.Haskell.Exts.Syntax  as S
+
+-------------------------------------------------------------------------------
+-- Aliases for Names in the Environment                                      --
+-------------------------------------------------------------------------------
+
+-- | The name of a data type.
+type TypeName = String
+
+-- | The name of a data constructor.
+type ConName = S.QName ()
+
+-------------------------------------------------------------------------------
+-- Environment Entries                                                       --
+-------------------------------------------------------------------------------
+
+-- | An entry of the 'Environment' for a data constructor that is in scope.
+data ConEntry = ConEntry
+  { conEntryName    :: ConName
+    -- ^ The name of the constructor.
+  , conEntryArity   :: Int
+    -- ^ The number of fields of the constructor.
+  , conEntryIsInfix :: Bool
+    -- ^ Whether the constructor should be written in infix notation or not.
+  , conEntryType    ::  TypeName
+    -- ^ The name of the data type that the constructor belongs to.
+  }
+
+-------------------------------------------------------------------------------
+-- Environment                                                               --
+-------------------------------------------------------------------------------
+
 -- | A data type for the state of the pattern matching compiler.
 data Environment = Environment
+  { envConEntries :: Map TypeName [ConEntry]
+  }
 
+-- | An empty 'Environment'.
 emptyEnv :: Environment
-emptyEnv = Environment
+emptyEnv = Environment { envConEntries = Map.empty }
