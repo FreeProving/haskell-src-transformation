5c9ee82306f5d843c72eb01ab7b51f34d7b916bc
Justin Andresen <jan.dresen.95@gmail.com>
2020-07-13 16:18:43 +0200

    Add convenience actions for `Fresh` #31
    Currently there is only an action to generate an indentifier
    for a fresh variable.
    However, it is often necessary to generate fresh varibale
    expressions or patterns. Additional actions with fixed
    interpretations have been added to generate such AST nodes.

--- src/lib/HST/Effect/Fresh.hs
+++ src/lib/HST/Effect/Fresh.hs
@@ -8,15 +8,22 @@ module HST.Effect.Fresh
   ( -- * Effect
     Fresh
     -- * Actions
+  , freshIdent
+  , freshName
+  , freshQName
   , freshVar
+  , freshVarPat
     -- * Interpretations
   , runFresh
+    -- * Backward Compatibility
+  , genericFreshPrefix
   )
 where
 
 import           Data.Map                       ( Map )
 import qualified Data.Map                      as Map
-import           Polysemy                       ( Sem
+import           Polysemy                       ( Member
+                                                , Sem
                                                 , makeSem
                                                 , reinterpret
                                                 )
@@ -26,15 +33,33 @@ import           Polysemy.State                 ( State
                                                 , modify
                                                 )
 
+import qualified HST.Frontend.Syntax           as S
+
 -------------------------------------------------------------------------------
 -- Effect and Actions                                                        --
 -------------------------------------------------------------------------------
 
 -- | An effect capable of generating names for a fresh variable.
 data Fresh m a where
-  FreshVar ::String -> Fresh m String
+  FreshIdent ::String -> Fresh m String
 
 makeSem ''Fresh
+
+-- | Generates a name for a fresh variable.
+freshName :: Member Fresh r => String -> Sem r (S.Name a)
+freshName prefix = S.Ident S.NoSrcSpan <$> freshIdent prefix
+
+-- | Generates an unqualified name for a fresh variable.
+freshQName :: Member Fresh r => String -> Sem r (S.QName a)
+freshQName prefix = S.UnQual S.NoSrcSpan <$> freshName prefix
+
+-- | Generates a fresh variable expression.
+freshVar :: Member Fresh r => String -> Sem r (S.Exp a)
+freshVar prefix = S.Var S.NoSrcSpan <$> freshQName prefix
+
+-- | Generates a fresh variable pattern.
+freshVarPat :: Member Fresh r => String -> Sem r (S.Pat a)
+freshVarPat prefix = S.PVar S.NoSrcSpan <$> freshName prefix
 
 -------------------------------------------------------------------------------
 -- Interpretations                                                           --
@@ -48,7 +73,18 @@ runFresh = evalState Map.empty . freshToState
   -- | Reinterprets 'Fresh' in terms of 'State'.
   freshToState :: Sem (Fresh ': r) a -> Sem (State (Map String Int) ': r) a
   freshToState = reinterpret \case
-    FreshVar prefix -> do
+    FreshIdent prefix -> do
       nextId <- gets $ Map.findWithDefault (0 :: Int) prefix
       modify $ Map.insert prefix (nextId + 1)
       return (prefix ++ show nextId)
+
+-------------------------------------------------------------------------------
+-- Backward Compatibility                                                    --
+-------------------------------------------------------------------------------
+
+-- | The default prefix to use for fresh variables.
+--
+--   Uses of this prefix should be replaced by more specific fresh variable
+--   prefixes.
+genericFreshPrefix :: String
+genericFreshPrefix = "a"
