987b09949f9caddf47aeffa169dc3129660a5fa2
Justin Andresen <jan.dresen.95@gmail.com>
2020-07-15 20:28:14 +0200

    Remove `error` calls from `HST.Environment.Renaming` #38 #17
    The removal of the catch all clause forces pattern matching
    in `HST.Environment.Renaming` to be exhaustive. Explicit
    handling of `S.NegApp` expressions fixes the example in #17.
    There may be more places where `S.NegApp`s need to be handled.

--- src/lib/HST/Environment/Renaming.hs
+++ src/lib/HST/Environment/Renaming.hs
@@ -43,18 +43,17 @@ instance TermSubst S.Exp where
     S.Lit l literal -> S.Lit l literal
     S.InfixApp l e1 qop e2 ->
       S.InfixApp l (substitute s e1) qop (substitute s e2)
+    S.NegApp l e     -> S.NegApp l (substitute s e)
     S.App    l e1 e2 -> S.App l (substitute s e1) (substitute s e2)
     S.Lambda l ps e  -> S.Lambda l ps (substitute s e)
     S.Let    l b  e  -> S.Let l b $ substitute s e
     S.If l e1 e2 e3 ->
       S.If l (substitute s e1) (substitute s e2) (substitute s e3)
-    S.Case  l e   as   -> S.Case l e (map (substitute s) as)
-    -- TODO no subst for debugging (substitute s e)
+    S.Case  l e   as   -> S.Case l (substitute s e) (map (substitute s) as)
     S.Tuple l bxd es   -> S.Tuple l bxd (map (substitute s) es)
     S.List  l es       -> S.List l (map (substitute s) es)
     S.Paren l e        -> S.Paren l (substitute s e)
     S.ExpTypeSig l e t -> S.ExpTypeSig l (substitute s e) t
-    _                  -> error "TermSubst: Exp caused an error"
 
 -- | 'TermSubst' instance for @case@ expression alternatives.
 --
@@ -68,7 +67,7 @@ instance TermSubst S.Alt where
 --   There must be no guards.
 instance TermSubst S.Rhs where
   substitute s rhs = case rhs of
-    S.UnGuardedRhs l e -> S.UnGuardedRhs l $ substitute s e
+    S.UnGuardedRhs l e -> S.UnGuardedRhs l (substitute s e)
     S.GuardedRhss  _ _ -> error "TermSubst: GuardedRhss not supported"
 
 -- | Type class for AST nodes of type @c@ a 'Subst' can be applied to.
@@ -78,12 +77,12 @@ class Rename c where
 
 -- | 'Rename' instance for expressions.
 instance Rename (S.Exp a) where
-    -- rename :: Subst -> Exp l -> Exp l
   rename s expr = case expr of
     S.Var l qname          -> S.Var l (rename s qname)
     S.Con l qname          -> S.Con l qname
     S.Lit l literal        -> S.Lit l literal
     S.InfixApp l e1 qop e2 -> S.InfixApp l (rename s e1) qop (rename s e2)
+    S.NegApp l e           -> S.NegApp l (rename s e)
     S.App    l e1 e2       -> S.App l (rename s e1) (rename s e2)
     S.Lambda l ps e        -> S.Lambda l (map (rename s) ps) (rename s e)
     S.Let    l b  e        -> S.Let l b $ rename s e
@@ -93,7 +92,6 @@ instance Rename (S.Exp a) where
     S.List  l es           -> S.List l (map (rename s) es)
     S.Paren l e            -> S.Paren l (rename s e)
     S.ExpTypeSig l e t     -> S.ExpTypeSig l (rename s e) t
-    _                      -> error "Rename: Exp caused an error"
 
 -- | 'Rename' instance for optionally qualified variable names.
 instance Rename (S.QName a) where
@@ -119,20 +117,24 @@ instance Rename (S.Alt s) where
 -- | 'Rename' instance for patterns.
 instance Rename (S.Pat a) where
   rename s pat = case pat of
-    S.PVar l name -> S.PVar l $ rename s name
+    S.PVar l name -> S.PVar l (rename s name)
     S.PInfixApp l p1 qname p2 ->
       S.PInfixApp l (rename s p1) (rename s qname) (rename s p2)
     S.PApp   l qname ps -> S.PApp l (rename s qname) (map (rename s) ps)
     S.PTuple l boxed ps -> S.PTuple l boxed (map (rename s) ps)
     S.PList  l ps       -> S.PList l (map (rename s) ps)
-    S.PParen l p        -> S.PParen l $ rename s p
+    S.PParen l p        -> S.PParen l (rename s p)
     S.PWildCard l       -> S.PWildCard l
 
 -- | 'Rename' instance for right-hand sides.
 --
---   Applies the substitution to the expression on the right-hand side.
---   There must be no guards.
+--   Applies the substitution to guards and the expression on the right-hand
+--   sides.
 instance Rename (S.Rhs s) where
   rename s rhs = case rhs of
-    S.UnGuardedRhs l e -> S.UnGuardedRhs l $ rename s e
-    S.GuardedRhss  _ _ -> error "Rename: GuardedRhss not supported"
+    S.UnGuardedRhs l e    -> S.UnGuardedRhs l (rename s e)
+    S.GuardedRhss  l rhss -> S.GuardedRhss l (map (rename s) rhss)
+
+-- | 'Rename' instance for right-hand sides with guards.
+instance Rename (S.GuardedRhs s) where
+  rename s (S.GuardedRhs l e1 e2) = S.GuardedRhs l (rename s e1) (rename s e2)
