fb2a1a8d39db06cf8df29aa910cf0391822f31fc
Justin Andresen <jan.dresen.95@gmail.com>
2020-07-21 16:46:23 +0200

    Add error reporting lookup functions #38
    When looking up entries in the `Environment` the result is
    wrapped with `Maybe` because the type or constructor may not
    be in scope. Utility functions have been added that report
    a fatal message when `Nothing` is returned.

---
+++ src/lib/HST/Environment/LookupOrReport.hs
@@ -0,0 +1,61 @@
+-- | This module contains variations of the "HST.Environment" lookup functions
+--   that don't return @Maybe@ values but report fatal error if a data or type
+--   constructor cannot be found in the environment.
+
+module HST.Environment.LookupOrReport
+  ( lookupConEntryOrReport
+  , lookupDataEntryOrReport
+  )
+where
+
+import           Polysemy                       ( Members
+                                                , Sem
+                                                )
+
+import           HST.Effect.Env                 ( Env
+                                                , inEnv
+                                                )
+import           HST.Effect.Report              ( Message(Message)
+                                                , Report
+                                                , Severity(Error)
+                                                , reportFatal
+                                                )
+import           HST.Environment                ( ConEntry
+                                                , DataEntry
+                                                , lookupConEntry
+                                                , lookupDataEntry
+                                                )
+import qualified HST.Frontend.Syntax           as S
+import           HST.Util.PrettyName            ( prettyName )
+
+-- | Looks up the entry of a data constructor with the given name in the
+--   current environment.
+--
+--   Reports a fatal error if the constructor could not be found.
+lookupConEntryOrReport
+  :: Members '[Env a, Report] r => S.QName a -> Sem r (ConEntry a)
+lookupConEntryOrReport conName = do
+  maybeConEntry <- inEnv $ lookupConEntry conName
+  case maybeConEntry of
+    Nothing ->
+      reportFatal
+        $  Message Error
+        $  "Data constructor not in scope: "
+        ++ prettyName conName
+    Just conEntry -> return conEntry
+
+-- | Looks up the entry of a type constructor with the given name in the
+--   current environment.
+--
+--   Reports a fatal error if the constructor could not be found.
+lookupDataEntryOrReport
+  :: Members '[Env a, Report] r => S.QName a -> Sem r (DataEntry a)
+lookupDataEntryOrReport dataName = do
+  maybeDataEntry <- inEnv $ lookupDataEntry dataName
+  case maybeDataEntry of
+    Nothing ->
+      reportFatal
+        $  Message Error
+        $  "Type constructor not in scope: "
+        ++ prettyName dataName
+    Just dataEntry -> return dataEntry
