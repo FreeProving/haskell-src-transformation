6e45201504f7b1f1bbe78caa861d49dfd9e467c8
Justin Andresen <jan.dresen.95@gmail.com>
2020-07-22 16:19:16 +0200

    Add `Data.Map.Strict` to `.hlint.yaml` #38

--- src/lib/HST/Effect/PatternStack.hs
+++ src/lib/HST/Effect/PatternStack.hs
@@ -16,8 +16,8 @@ module HST.Effect.PatternStack
   )
 where
 
-import           Data.Map                       ( Map )
-import qualified Data.Map                      as Map
+import           Data.Map.Strict                ( Map )
+import qualified Data.Map.Strict               as Map
 import           Polysemy                       ( Sem
                                                 , makeSem
                                                 , reinterpret
--- src/lib/HST/Environment.hs
+++ src/lib/HST/Environment.hs
@@ -17,8 +17,8 @@ module HST.Environment
   )
 where
 
-import           Data.Map                       ( Map )
-import qualified Data.Map                      as Map
+import           Data.Map.Strict                ( Map )
+import qualified Data.Map.Strict               as Map
 
 import qualified HST.Frontend.Syntax           as S
 
