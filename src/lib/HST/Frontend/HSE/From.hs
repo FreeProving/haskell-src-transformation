44581ff63f39283d8a5a616db442b51eea89dd51
Leif-Erik Krüger <stu204767@mail.uni-kiel.de>
2020-07-30 20:43:39 +0200

    Move functions for AST transformation error messages to `Effect/Report.hs` #33

--- src/lib/HST/Frontend/HSE/From.hs
+++ src/lib/HST/Frontend/HSE/From.hs
@@ -10,11 +10,9 @@ import           Polysemy                       ( Member
                                                 , Sem
                                                 )
 
-import           HST.Effect.Report              ( Message(Message)
-                                                , Report
-                                                , Severity(Error, Info)
-                                                , report
-                                                , reportFatal
+import           HST.Effect.Report              ( Report
+                                                , notSupported
+                                                , skipNotSupported
                                                 )
 import           HST.Frontend.HSE.Config        ( HSE
                                                 , OriginalModuleHead
@@ -22,27 +20,6 @@ import           HST.Frontend.HSE.Config        ( HSE
                                                   )
                                                 )
 import qualified HST.Frontend.Syntax           as S
-
-------------------------------------------------------------------------------
--- Error messages                                                           --
-------------------------------------------------------------------------------
-
--- | Reports a fatal error that the given feature is not supported.
-notSupported
-  :: Member Report r
-  => String -- ^ The name of the feature (plural) that is not supported.
-  -> Sem r b
-notSupported feature =
-  reportFatal $ Message Error $ feature ++ " are not supported!"
-
--- | Informs the user that the given feature is not supported and the
---   corresponding AST node will be skipped.
-skipNotSupported
-  :: Member Report r
-  => String -- ^ The name of the feature (plural) that is not supported.
-  -> Sem r ()
-skipNotSupported feature =
-  report $ Message Info $ feature ++ " are not supported and will be skipped!"
 
 -------------------------------------------------------------------------------
 -- Modules                                                                   --
