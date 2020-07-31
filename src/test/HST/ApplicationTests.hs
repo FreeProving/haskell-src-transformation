bfc106d078c50283148903086f1b6fa6d74329fd
Justin Andresen <jan.dresen.95@gmail.com>
2020-07-31 13:50:03 +0200

    Format code in test cases #43

--- src/test/HST/ApplicationTests.hs
+++ src/test/HST/ApplicationTests.hs
@@ -126,14 +126,14 @@ testProcessModule = context "processModule" $ do
     shouldTransformTo
       [ "module A where"
       , "lengthL :: [a] -> Int"
-      , "lengthL [] = 0"
-      , "lengthL(_:xs) = 1 + lengthL xs"
+      , "lengthL []       = 0"
+      , "lengthL (_ : xs) = 1 + lengthL xs"
       ]
       [ "module A where"
       , "lengthL :: [a] -> Int"
       , "lengthL a0 = case a0 of"
-      , "  []    -> 0"
-      , "  a1:a2 -> 1 + lengthL a2"
+      , "  []      -> 0"
+      , "  a1 : a2 -> 1 + lengthL a2"
       ]
   it "should transform pattern matching in a partial function" $ runTest $ do
     shouldTransformTo
@@ -142,18 +142,18 @@ testProcessModule = context "processModule" $ do
       , "head :: [a] -> a"
       , "head a0 = case a0 of"
       , "  a1 : a2 -> a1"
-      , "  [] -> undefined"
+      , "  []      -> undefined"
       ]
   it "should accept a simple guarded expression" $ runTest $ do
     shouldTransformTo
       ["module A where", "id :: a -> a", "id x | otherwise = x"]
       [ "module A where"
       , "id :: a -> a"
-      , "id a0 = let a1 = case a0 of"
-      , "              a3 -> if otherwise then a3"
-      , "                                 else a2"
-      , "            a2 = undefined"
-      , "        in a1"
+      , "id a0 ="
+      , "  let a1 = case a0 of"
+      , "        a3 -> if otherwise then a3 else a2"
+      , "      a2 = undefined"
+      , "  in  a1"
       ]
   it "should accept a more complex guarded function" $ runTest $ do
     shouldTransformTo
@@ -168,9 +168,7 @@ testProcessModule = context "processModule" $ do
       , "  let a3 = case a0 of"
       , "        a5 -> case a1 of"
       , "          a6 -> case a2 of"
-      , "            a7 -> if a5 a6 then a6"
-      , "                           else if otherwise then a7"
-      , "                                             else a4"
+      , "            a7 -> if a5 a6 then a6 else if otherwise then a7 else a4"
       , "      a4 = undefined"
-      , "  in a3"
+      , "  in  a3"
       ]
