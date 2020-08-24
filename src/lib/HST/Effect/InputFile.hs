0db96bb0816aac523f4feb15377b6ed314d5adbb
Leif-Erik Krüger <stu204767@mail.uni-kiel.de>
2020-08-24 22:48:49 +0200

    Use a map allowing to store multiple input files in the input file effect #36

--- src/lib/HST/Effect/InputFile.hs
+++ src/lib/HST/Effect/InputFile.hs
@@ -4,48 +4,50 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 -- | This module defines an effect for computations that can store the file
---   path and the content of an input file.
+--   path and the content of multiple input files.
 module HST.Effect.InputFile
   ( -- * Effect
     InputFile
     -- * Actions
-  , setFile
-  , getFile
+  , addInputFile
+  , getInputFile
     -- * Interpretations
   , runInputFile
   ) where
 
+import           Data.Map.Strict     ( Map )
+import qualified Data.Map.Strict     as Map
 import           Polysemy       ( Sem, makeSem, reinterpret )
 import           Polysemy.State ( State, evalState, get, put )
 
 -------------------------------------------------------------------------------
 -- Effect and Actions                                                        --
 -------------------------------------------------------------------------------
--- | An effect capable of setting and getting the file path and the content of
---   the input file.
+-- | An effect capable of adding and getting the file path and the content of
+--   input files.
 --
---   While setting the input file, the content is converted to lines of code.
+--   While setting an input file, the content is converted to lines of code.
 data InputFile m a where
-  SetFile :: FilePath -> String -> InputFile m ()
-  GetFile :: InputFile m (FilePath, [String])
+  AddInputFile :: FilePath -> String -> InputFile m ()
+  GetInputFile :: FilePath -> InputFile m (Maybe [String])
 
 makeSem ''InputFile
 
 -------------------------------------------------------------------------------
 -- Interpretations                                                           --
 -------------------------------------------------------------------------------
--- | Handles a computation by providing initially empty file data.
+-- | Handles a computation by providing an initially empty file map.
 runInputFile :: Sem (InputFile ': r) a -> Sem r a
-runInputFile = runWithInputFile ("", [])
+runInputFile = runWithInputFile Map.empty
 
--- | Handles a computation by providing the supplied file data to read from or
---   to set new file data.
-runWithInputFile :: (FilePath, [String]) -> Sem (InputFile ': r) a -> Sem r a
-runWithInputFile initialFileData = evalState initialFileData . inputFileToState
+-- | Handles a computation by providing the supplied file map to read from or
+--   to add new input files.
+runWithInputFile :: Map FilePath [String] -> Sem (InputFile ': r) a -> Sem r a
+runWithInputFile initialFileMap = evalState initialFileMap . inputFileToState
  where
   -- | Reinterprets 'InputFile' in terms of 'State'.
   inputFileToState
-    :: Sem (InputFile ': r) a -> Sem (State (FilePath, [String]) ': r) a
+    :: Sem (InputFile ': r) a -> Sem (State (Map FilePath [String]) ': r) a
   inputFileToState = reinterpret \case
-    SetFile path content -> put (path, lines content)
-    GetFile              -> get
+    AddInputFile path content -> get >>= put . Map.insert path (lines content)
+    GetInputFile path         -> get >>= return . Map.lookup path
