dc69e91e04e4e2c82da877742593e144f475fba2
Leif-Erik Krüger <stu204767@mail.uni-kiel.de>
2020-08-31 15:14:12 +0200

    Resolve type errors in the `HST.Frontend.Transformer.Messages` module #36

--- src/lib/HST/Frontend/Transformer/Messages.hs
+++ src/lib/HST/Frontend/Transformer/Messages.hs
@@ -2,12 +2,10 @@
 --   transformation of ASTs.
 module HST.Frontend.Transformer.Messages where
 
-import           Data.Char            ( isSpace )
-import           Polysemy             ( Member, Members, Sem )
+import           Polysemy             ( Member, Sem )
 
-import           HST.Effect.InputFile ( InputFile, getInputFile )
 import           HST.Effect.Report
-  ( Message(Message), Report, Severity(Error, Info), report, reportFatal )
+  ( Report, Severity(Error, Info), message, report, reportFatal )
 import qualified HST.Frontend.Syntax  as S
 
 -- | Reports a fatal error that the given feature is not supported.
@@ -16,7 +14,7 @@ notSupported
   => String -- ^ The name of the feature (plural) that is not supported.
   -> Sem r b
 notSupported feature
-  = reportFatal $ Message Error $ feature ++ " are not supported!"
+  = reportFatal $ message Error S.NoSrcSpan $ feature ++ " are not supported!"
 
 -- | Informs the user that the given feature is not supported and the
 --   corresponding AST node will be skipped.
@@ -25,31 +23,29 @@ skipNotSupported
   => String -- ^ The name of the feature (plural) that is not supported.
   -> Sem r ()
 skipNotSupported feature = report
-  $ Message Info
+  $ message Info S.NoSrcSpan
   $ feature ++ " are not supported and will be skipped!"
 
 -- | Reports a fatal error that the given feature is not supported. Displays an
 --   excerpt of the input code specified by the given source span.
 notSupportedWithExcerpt
-  :: Members '[InputFile, Report] r
+  :: Member Report r
   => String      -- ^ The name of the feature (plural) that is not supported.
   -> S.SrcSpan a -- ^ The source span of the excerpt to display.
   -> Sem r b
-notSupportedWithExcerpt feature srcSpan = do
-  excerpt <- displayCodeExcerpt srcSpan
-  reportFatal $ Message Error $ feature ++ " are not supported!\n" ++ excerpt
+notSupportedWithExcerpt feature srcSpan =
+  reportFatal $ message Error srcSpan $ feature ++ " are not supported!"
 
 -- | Informs the user that the given feature is not supported and the
 --   corresponding AST node will be skipped. Displays an excerpt of the input
 --   code specified by the given source span.
 skipNotSupportedWithExcerpt
-  :: Members '[InputFile, Report] r
+  :: Member Report r
   => String      -- ^ The name of the feature (plural) that is not supported.
   -> S.SrcSpan a -- ^ The source span of the excerpt to display.
   -> Sem r ()
-skipNotSupportedWithExcerpt feature srcSpan = do
-  excerpt <- displayCodeExcerpt srcSpan
+skipNotSupportedWithExcerpt feature srcSpan =
   report
-    $ Message Info
-    $ feature ++ " are not supported and will be skipped!\n" ++ excerpt
+    $ message Info srcSpan
+    $ feature ++ " are not supported and will be skipped!"
 
