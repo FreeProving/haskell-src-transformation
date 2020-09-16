d53d9f1b5388a5dc17cb7249a3fb7ff8c8b76226
stu204146-kiel <stu204146@mail.uni-kiel.de>
2020-09-16 11:00:53 +0200

    Format code #36

--- src/test/Spec.hs
+++ src/test/Spec.hs
@@ -8,10 +8,9 @@ import           HST.Effect.CancelTests  ( testCancelEffect )
 import           HST.Effect.FreshTests   ( testFreshEffect )
 import           HST.Effect.ReportTests  ( testReportEffect )
 import           HST.Util.FreeVarsTests  ( testFreeVars )
-import           HST.Util.MessagesTests ( testMessages )
+import           HST.Util.MessagesTests  ( testMessages )
 import           HST.Util.SelectorsTests ( testSelectors )
 import           HST.Util.SubstTests     ( testSubst )
-
 
 main :: IO ()
 main = hspec $ do
