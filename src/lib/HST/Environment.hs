936ebf056bff058380baa69cb650630af0b6ba83
Leif-Erik Krüger <stu204767@mail.uni-kiel.de>
2020-09-17 12:42:33 +0200

    Restructure the `Environment` module #11 - Module Interfaces are now used to store environment entries. - The lookup functions return lists now because of possible   ambiguities. - The insertion functions have been removed since there   currently is no need to insert entries into the environment.

--- src/lib/HST/Environment.hs
+++ src/lib/HST/Environment.hs
@@ -1,24 +1,21 @@
 -- | This module contains an abstract data type for the pattern matching
---   compiler's state.
+--   compiler's environment.
 module HST.Environment
   ( -- * Environment Entries
     DataEntry(..)
     -- * Environment
-  , Environment
-  , emptyEnv
+  , Environment(..)
     -- * Lookup
-  , lookupConEntry
-  , lookupDataEntry
-    -- * Insertion
-  , insertConEntry
-  , insertDataEntry
+  , lookupConEntries
+  , lookupTypeName
   ) where
 
-import           Data.Map.Strict        ( Map )
 import qualified Data.Map.Strict        as Map
+import           Data.Maybe             ( mapMaybe )
 
 import           HST.Effect.InputModule
-  ( ConEntry(conEntryName), ConName, TypeName )
+  ( ConEntry, ConName, ModuleInterface(..), TypeName )
+import qualified HST.Frontend.Syntax    as S
 
 -- | An entry of the 'Environment' for a data type whose constructors are in
 --   scope.
@@ -32,46 +29,38 @@ data DataEntry a = DataEntry
 -------------------------------------------------------------------------------
 -- Environment                                                               --
 -------------------------------------------------------------------------------
--- | A data type for the state of the pattern matching compiler.
+-- | A data type for the environment of the pattern matching compiler
+--   containing data types and data constructors currently in scope.
 data Environment a = Environment
-  { envConEntries  :: Map (ConName a) (ConEntry a)
-    -- ^ Maps names of constructors to their 'ConEntry's.
-  , envDataEntries :: Map (TypeName a) (DataEntry a)
-    -- ^ Maps names of data types to their 'DataEntry's.
+  { envCurrentModule :: ModuleInterface a
+    -- ^ The module interface for the current module, containing its data types
+    --   and data constructors.
+  , envImportedModules :: [(S.ImportDecl a, ModuleInterface a)]
+    -- ^ A list of all successfully imported module interfaces including their
+    --   import declarations. 
+  , envOtherEntries :: ModuleInterface a
+    -- ^ A module interface containing all other data types and data
+    --   constructors currently in scope.
   }
-
--- | An empty 'Environment'.
-emptyEnv :: Environment a
-emptyEnv
-  = Environment { envConEntries = Map.empty, envDataEntries = Map.empty }
 
 -------------------------------------------------------------------------------
 -- Lookup                                                                    --
 -------------------------------------------------------------------------------
--- | Looks up the entry of a data constructor with the given name in the
---   environment.
---
---   Returns @Nothing@ if the data constructor is not in scope.
-lookupConEntry :: ConName a -> Environment a -> Maybe (ConEntry a)
-lookupConEntry name = Map.lookup name . envConEntries
+-- | Looks up the constructors belonging to the the given data type name in the
+--   given environment.
+--   
+--   Returns an empty list if the data type name is not in scope.
+--   Returns multiple constructor lists if the data type name is ambiguous.
+lookupConEntries :: TypeName a -> Environment a -> [[ConEntry a]]
+lookupConEntries typeName env = mapMaybe (Map.lookup typeName . interfaceDataCons)
+  (envCurrentModule env : envOtherEntries env : map snd (envImportedModules env))
 
--- | Looks up the entry of a data type with the given name in the environment.
---
---   Returns @Nothing@ if the data type is not in scope.
-lookupDataEntry :: TypeName a -> Environment a -> Maybe (DataEntry a)
-lookupDataEntry name = Map.lookup name . envDataEntries
-
--------------------------------------------------------------------------------
--- Insertion                                                                 --
--------------------------------------------------------------------------------
--- | Inserts the given entry for a data constructor into the environment.
-insertConEntry :: ConEntry a -> Environment a -> Environment a
-insertConEntry entry env = env
-  { envConEntries = Map.insert (conEntryName entry) entry (envConEntries env)
-  }
-
--- | Inserts the given entry for a data type into the environment.
-insertDataEntry :: DataEntry a -> Environment a -> Environment a
-insertDataEntry entry env = env
-  { envDataEntries = Map.insert (dataEntryName entry) entry (envDataEntries env)
-  }
+-- | Looks up the data type names belonging to the the given data constructor
+--   name in the given environment.
+--   
+--   Returns an empty list if the data constructor name is not in scope.
+--   Returns multiple data type names if the data constructor name is
+--   ambiguous.
+lookupTypeName :: ConName a -> Environment a -> [TypeName a]
+lookupTypeName conName env = mapMaybe (Map.lookup conName . interfaceTypeNames)
+  (envCurrentModule env : envOtherEntries env : map snd (envImportedModules env))
