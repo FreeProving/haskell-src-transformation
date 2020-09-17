5726c18a00eb53893b92a3de7cf1431df7764e30
Leif-Erik Krüger <stu204767@mail.uni-kiel.de>
2020-09-17 18:37:46 +0200

    Adapt `Application.hs` to the new environment structure #11 A new function initializing the environment has been added in addition to the module interface creation function.

--- src/lib/HST/Application.hs
+++ src/lib/HST/Application.hs
@@ -1,32 +1,34 @@
 -- | This module applies the main pattern-matching compilation algorithm and
 --   the different features to a Haskell module.
-module HST.Application ( processModule, createModuleInterface ) where
+module HST.Application ( createModuleInterface, initializeEnvironment, processModule ) where
 
 -- TODO too many variables generated
 -- TODO only tuples supported
-import           Control.Monad                ( replicateM )
+import           Control.Monad                ( replicateM, zipWithM )
 import           Control.Monad.Extra          ( ifM )
 import qualified Data.Map.Strict              as Map
-import           Data.Maybe                   ( mapMaybe )
+import           Data.Maybe                   ( catMaybes, mapMaybe )
 import           Polysemy                     ( Member, Members, Sem )
 
 import           HST.CoreAlgorithm            ( Eqs, defaultErrorExp, match )
-import           HST.Effect.Env               ( Env, modifyEnv )
+import           HST.Effect.Env               ( Env )
 import           HST.Effect.Fresh
   ( Fresh, freshVarPat, genericFreshPrefix )
 import           HST.Effect.GetOpt            ( GetOpt, getOpt )
 import           HST.Effect.InputModule
-  ( ConEntry(..), ConName, ModuleInterface(ModuleInterface), TypeName )
-import           HST.Effect.Report            ( Report )
+  ( ConEntry(..), InputModule, ModuleInterface(..), TypeName, getInputModule, getInputModuleInterface, getInputModuleInterfaceByName, revertInterfaceEntry )
+import           HST.Effect.Report            ( Report, report )
 import           HST.Environment
-  ( DataEntry(..), insertConEntry, insertDataEntry )
-import           HST.Environment.Prelude      ( insertPreludeEntries )
+  ( Environment(..) )
+import           HST.Environment.Prelude      ( preludeModuleInterface )
 import           HST.Feature.CaseCompletion   ( applyCCModule )
 import           HST.Feature.GuardElimination ( applyGEModule, getMatchName )
 import           HST.Feature.Optimization     ( optimize )
 import qualified HST.Frontend.Syntax          as S
 import           HST.Options                  ( optOptimizeCase )
+import           HST.Util.Messages            ( Severity(Info), message )
 import           HST.Util.Predicates          ( isConPat )
+import           HST.Util.PrettyName          ( prettyName )
 import           HST.Util.Selectors           ( expFromUnguardedRhs )
 
 -------------------------------------------------------------------------------
@@ -41,8 +43,6 @@ processModule :: (Members '[Env a, Fresh, GetOpt, Report] r, S.EqAST a)
               => S.Module a
               -> Sem r (S.Module a)
 processModule m = do
-  insertPreludeEntries
-  collectDataInfo m
   guardEliminatedM <- applyGEModule m
   caseCompletedM <- applyCCModule guardEliminatedM
   useAlgoModule caseCompletedM
@@ -107,26 +107,18 @@ useAlgo ms = do
     return (pat : pats, expr)
 
 -------------------------------------------------------------------------------
--- Module Interface Initialization                                           --
+-- Module Interface Creation                                                 --
 -------------------------------------------------------------------------------
 -- | Creates a module interface with the data types declared in the given
 --   module.
 createModuleInterface :: S.Module a -> ModuleInterface a
 createModuleInterface (S.Module _ _ modName _ decls) =
   let interfaceEntries = mapMaybe createModuleInterfaceEntry decls
-      revertedEntries = concatMap revertEntry interfaceEntries
-  in ModuleInterface modName
-                     (Map.fromList interfaceEntries)
-                     (Map.fromList revertedEntries)
- where
-  -- | Converts a map entry mapping a data type name to constructor entries to
-  --   a list of map entries mapping the names of the given constructors to the
-  --   given type name.
-  revertEntry :: (TypeName a, [ConEntry a]) -> [(ConName a, TypeName a)]
-  revertEntry (typeName, conEntry : conEntries) =
-    (conEntryName conEntry, typeName)
-      : revertEntry (typeName, conEntry : conEntries)
-  revertEntry (_, []) = []
+      revertedEntries = concatMap revertInterfaceEntry interfaceEntries
+  in ModuleInterface { interfaceModName = modName
+                     , interfaceDataCons = Map.fromList interfaceEntries
+                     , interfaceTypeNames = Map.fromList revertedEntries
+                     }
 
 -- | Creates a module interface entry for the data type and constructors
 --   declared by the given declaration.
@@ -139,31 +131,7 @@ createModuleInterfaceEntry (S.DataDecl _ _ dataName conDecls)
     in Just (dataQName, conEntries)
 createModuleInterfaceEntry _ = Nothing
 
--------------------------------------------------------------------------------
--- Environment Initialization                                                --
--------------------------------------------------------------------------------
--- | Initializes the environment with the data types declared in the given
---   module.
-collectDataInfo :: Member (Env a) r => S.Module a -> Sem r ()
-collectDataInfo (S.Module _ _ _ _ decls) = mapM_ collectDataDecl decls
-
--- | Inserts entries for the data type and constructors declared by the given
---   declaration into the environment.
---
---   Leaves the environment unchanged, if the given declaration is not a
---   data type declaration.
-collectDataDecl :: Member (Env a) r => S.Decl a -> Sem r ()
-collectDataDecl (S.DataDecl _ _ dataName conDecls) = do
-  let dataQName  = S.UnQual S.NoSrcSpan dataName
-      conEntries = map (makeConEntry dataQName) conDecls
-  modifyEnv
-    $ insertDataEntry DataEntry { dataEntryName = dataQName
-                                , dataEntryCons = map conEntryName conEntries
-                                }
-  mapM_ (modifyEnv . insertConEntry) conEntries
-collectDataDecl _ = return ()
-
--- | Creates an environment entry for a constructor declaration.
+-- | Creates a module interface entry for a constructor declaration.
 makeConEntry :: S.QName a -> S.ConDecl a -> ConEntry a
 makeConEntry dataQName conDecl = ConEntry
   { conEntryName    = S.UnQual S.NoSrcSpan (S.conDeclName conDecl)
@@ -171,3 +139,37 @@ makeConEntry dataQName conDecl = ConEntry
   , conEntryIsInfix = S.conDeclIsInfix conDecl
   , conEntryType    = dataQName
   }
+
+-------------------------------------------------------------------------------
+-- Environment Initialization                                                --
+-------------------------------------------------------------------------------
+-- | Initializes the environment for the module stored at the given file path.
+--
+--   The module interfaces of imported modules are added to the environment.
+--   If such a module is unavailable, the import is skipped and the user is
+--   informed about the skip.
+initializeEnvironment :: Members '[InputModule a, Report] r
+                      => FilePath -> Sem r (Environment a)
+initializeEnvironment filePath = do
+  S.Module _ _ _ imports _ <- getInputModule filePath
+  currentModule <- getInputModuleInterface filePath
+  mInterfaces <- mapM (getInputModuleInterfaceByName . S.importModule) imports
+  mImportedModules <- zipWithM reportMissingModule imports mInterfaces
+  return Environment { envCurrentModule   = currentModule
+                     , envImportedModules = catMaybes mImportedModules
+                     , envOtherEntries    = preludeModuleInterface
+                     }
+ where
+  -- | Returns the given import declaration and module interface, if the module
+  --   could be successfully be imported, or reports the skip of the missing
+  --   module and returns @Nothing@.
+  reportMissingModule
+    :: Member Report r => S.ImportDecl a -> Maybe (ModuleInterface a)
+                       -> Sem r (Maybe (S.ImportDecl a, ModuleInterface a))
+  reportMissingModule importDecl Nothing = do
+    report $ message Info (S.importSrcSpan importDecl) $
+          "The module `" ++ prettyName (S.importModule importDecl)
+       ++ "` could not be found and the import is skipped!"
+    return Nothing
+  reportMissingModule importDecl (Just moduleInterface) =
+    return $ Just (importDecl, moduleInterface)
