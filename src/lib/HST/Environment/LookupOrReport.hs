13d3cbf24b1870a70ec4cc56b2f94f8f0f399e31
Leif-Erik Krüger <stu204767@mail.uni-kiel.de>
2020-09-17 17:35:11 +0200

    Adapt `Environment/LookupOrReport.hs` to the new environment structure #11

--- src/lib/HST/Environment/LookupOrReport.hs
+++ src/lib/HST/Environment/LookupOrReport.hs
@@ -1,46 +1,71 @@
 -- | This module contains variations of the "HST.Environment" lookup functions
---   that don't return @Maybe@ values but report a fatal error if a data or
---   type constructor cannot be found in the environment.
+--   that remove the list and pair constructor wrapping the return type.
+--
+--   If the original function does not return a singleton list, a fatal error
+--   mentioning the out-of-scope or ambiguous entry is reported.
 module HST.Environment.LookupOrReport
-  ( lookupConEntryOrReport
-  , lookupDataEntryOrReport
+  ( lookupConEntriesOrReport
+  , lookupTypeNameOrReport
   ) where
 
+import           Data.Maybe             ( fromMaybe )
+import           Data.List              ( intercalate )
 import           Polysemy               ( Members, Sem )
 
 import           HST.Effect.Env         ( Env, inEnv )
-import           HST.Effect.InputModule ( ConEntry )
+import           HST.Effect.InputModule ( ConName, ConEntry, TypeName )
 import           HST.Effect.Report      ( Report, reportFatal )
 import           HST.Environment
-  ( DataEntry, lookupConEntry, lookupDataEntry )
+  ( lookupConEntries, lookupTypeName )
 import qualified HST.Frontend.Syntax    as S
 import           HST.Util.Messages      ( Severity(Error), message )
 import           HST.Util.PrettyName    ( prettyName )
 
--- | Looks up the entry of a data constructor with the given name in the
+-- | Looks up the constructors belonging to the the given data type name in the
 --   current environment.
---
---   Reports a fatal error if the constructor could not be found.
-lookupConEntryOrReport
-  :: Members '[Env a, Report] r => S.QName a -> Sem r (ConEntry a)
-lookupConEntryOrReport conName = do
-  maybeConEntry <- inEnv $ lookupConEntry conName
-  case maybeConEntry of
-    Nothing       -> reportFatal
-      $ message Error S.NoSrcSpan
-      $ "Data constructor not in scope: " ++ prettyName conName
-    Just conEntry -> return conEntry
+--   
+--   Reports a fatal error if the data type could not be found or if it could
+--   be found in multiple modules. In the latter case, the names of
+--   these modules are displayed as part of the error message.
+lookupConEntriesOrReport :: Members '[Env a, Report] r => TypeName a -> Sem r [ConEntry a]
+lookupConEntriesOrReport typeName = do
+  conEntriess <- inEnv $ lookupConEntries typeName
+  case conEntriess of
+    []                -> reportFatal
+      $ message Error (S.getSrcSpan typeName)
+      $ "Data type not in scope: " ++ prettyName typeName
+    [(_, conEntries)] -> return conEntries
+    _                 -> reportFatal
+      $ message Error (S.getSrcSpan typeName)
+      $    "Ambiguous data type `" ++ prettyName typeName
+        ++ "` could refer to one of the following modules: "
+        ++ displayModuleNames conEntriess
 
--- | Looks up the entry of a type constructor with the given name in the
---   current environment.
+-- | Looks up the data type names belonging to the the given data constructor
+--   name in the current environment.
+--   
+--   Reports a fatal error if the data constructor could not be found or if it
+--   could be found in multiple modules. In the latter case, the names of these
+--   modules are displayed as part of the error message.
+lookupTypeNameOrReport :: Members '[Env a, Report] r => ConName a -> Sem r (TypeName a)
+lookupTypeNameOrReport conName = do
+  typeNames <- inEnv $ lookupTypeName conName
+  case typeNames of
+    []              -> reportFatal
+      $ message Error (S.getSrcSpan conName)
+      $ "Data constructor not in scope: " ++ prettyName conName
+    [(_, typeName)] -> return typeName
+    _               -> reportFatal
+      $ message Error (S.getSrcSpan conName)
+      $    "Ambiguous data constructor `" ++ prettyName conName
+        ++ "` could refer to one of the following modules: "
+        ++ displayModuleNames typeNames
+
+-- | Display the module names contained in the first pair entries of the given
+--   list.
 --
---   Reports a fatal error if the constructor could not be found.
-lookupDataEntryOrReport
-  :: Members '[Env a, Report] r => S.QName a -> Sem r (DataEntry a)
-lookupDataEntryOrReport dataName = do
-  maybeDataEntry <- inEnv $ lookupDataEntry dataName
-  case maybeDataEntry of
-    Nothing        -> reportFatal
-      $ message Error S.NoSrcSpan
-      $ "Type constructor not in scope: " ++ prettyName dataName
-    Just dataEntry -> return dataEntry
+--   As no modules without a module name should be able to be imported
+--   successfully, missing module names are replaced with @This module@.
+displayModuleNames :: [(Maybe (S.ModuleName a), b)] -> String
+displayModuleNames = intercalate ", " . map
+                     (fromMaybe "This module" . fmap prettyName . fst)
