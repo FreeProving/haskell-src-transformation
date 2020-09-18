95a97d71ca45951db2bf06c414872b73ba1bf0fe
Leif-Erik Krüger <stu204767@mail.uni-kiel.de>
2020-09-18 23:45:04 +0200

    Implement the support for qualified identifiers for the environment lookup functions #12

--- src/lib/HST/Environment.hs
+++ src/lib/HST/Environment.hs
@@ -10,6 +10,7 @@ module HST.Environment
   , lookupTypeName
   ) where
 
+import           Data.Map.Strict        ( Map )
 import qualified Data.Map.Strict        as Map
 import           Data.Maybe             ( fromMaybe, mapMaybe )
 
@@ -46,12 +47,7 @@ data Environment a = Environment
 --   is ambiguous.
 lookupConEntries
   :: TypeName a -> Environment a -> [(Maybe (S.ModuleName a), [ConEntry a])]
-lookupConEntries typeName env = mapMaybe
-  (\x ->
-   fmap (interfaceModName x, ) (Map.lookup typeName (interfaceDataCons x)))
-  (envCurrentModule env
-   : envOtherEntries env
-   : map snd (envImportedModules env))
+lookupConEntries = lookupWith interfaceDataCons
 
 -- | Looks up the data type names belonging to the the given data constructor
 --   name in the given environment. The result includes the names of the
@@ -62,18 +58,35 @@ lookupConEntries typeName env = mapMaybe
 --   is ambiguous.
 lookupTypeName
   :: ConName a -> Environment a -> [(Maybe (S.ModuleName a), TypeName a)]
-lookupTypeName conName env = mapMaybe
-  (\x ->
-   fmap (interfaceModName x, ) (Map.lookup conName (interfaceTypeNames x)))
-  (envCurrentModule env
-   : envOtherEntries env
-   : map snd (envImportedModules env))
+lookupTypeName = lookupWith interfaceTypeNames
 
 -------------------------------------------------------------------------------
 -- Lookup Utility Functions                                                  --
 -------------------------------------------------------------------------------
--- | Checks if the given qualified name could refer to an entry of the given
---   module interface.
+-- | A generic version of 'lookupConEntries' and 'lookupTypeName' that
+--   additionally takes a function mapping a module interface to the desired
+--   map, i. e. one of the field names of 'ModuleInterface', as its first
+--   argument.
+lookupWith :: (ModuleInterface a -> Map (S.QName a) v)
+           -> S.QName a
+           -> Environment a
+           -> [(Maybe (S.ModuleName a), v)]
+lookupWith getMap qName env = mapMaybe
+  (\i ->
+    fmap (interfaceModName i, ) (Map.lookup (unQualifyName qName) (getMap i)))
+  (possibleInterfaces qName env)
+
+-- | Returns the list of all module interfaces in the given environment the
+--   given possibly qualified name could refer to.
+possibleInterfaces :: S.QName a -> Environment a -> [ModuleInterface a]
+possibleInterfaces qName env =
+     filter
+       (fitsToInterface qName)
+       [envCurrentModule env, envOtherEntries env]
+  ++ map snd (filter (fitsToImport qName . fst) (envImportedModules env))
+
+-- | Checks if the given possibly qualified name could refer to an entry of the
+--   given module interface.
 --
 --   It is assumed that the given module interface is imported unqualified
 --   since it should either belong to the current module or implicitly imported
@@ -83,8 +96,8 @@ fitsToInterface :: S.QName a -> ModuleInterface a -> Bool
 fitsToInterface (S.Qual _ modName _) = elem modName . interfaceModName
 fitsToInterface _ = const True
 
--- | Checks if the given qualified name could refer to an entry of a module
---   imported with the given import declaration.
+-- | Checks if the given possibly qualified name could refer to an entry of a
+--   module imported with the given import declaration.
 fitsToImport :: S.QName a -> S.ImportDecl a -> Bool
 fitsToImport (S.Qual _ modName _) importDecl = modName ==
   fromMaybe (S.importModule importDecl) (S.importAsName importDecl)
@@ -92,7 +105,7 @@ fitsToImport _ importDecl = not (S.importIsQual importDecl)
 
 -- | Removes the possible qualification of the given 'S.QName'.
 --
---   Other 'S.QName's, including special names for built-in data constructors
+--   Other 'S.QName's, including special names for built-in data constructors,
 --   are returned as given.
 unQualifyName :: S.QName a -> S.QName a
 unQualifyName (S.Qual s _ name) = S.UnQual s name
