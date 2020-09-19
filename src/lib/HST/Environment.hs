b0bd0d2b8acc576433e7de9ad2008a9018abed39
Leif-Erik Krüger <stu204767@mail.uni-kiel.de>
2020-09-20 00:31:41 +0200

    Add and update comments for environment lookup functions #12

--- src/lib/HST/Environment.hs
+++ src/lib/HST/Environment.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE TupleSections #-}
 
 -- | This module contains an abstract data type for the pattern matching
---   compiler's environment.
+--   compiler's environment and functions for looking up entries in this
+--   environment.
 module HST.Environment
   ( -- * Environment
     Environment(..)
@@ -38,13 +39,17 @@ data Environment a = Environment
 -------------------------------------------------------------------------------
 -- Lookup                                                                    --
 -------------------------------------------------------------------------------
--- | Looks up the constructors belonging to the the given data type name in the
---   given environment. The result includes the names of the modules the
---   constructors came from, if available.
+-- | Looks up the data constructor entries belonging to the the given data type
+--   name in the given environment and qualifies them so that they are
+--   unambiguous, if possible. The result includes the names of the modules
+--   the constructors came from, where available.
 --
 --   Returns an empty list if the data type name is not in scope.
---   Returns multiple module names and constructor lists if the data type name
---   is ambiguous.
+--   Returns a single pair with the second entry being @Nothing@ if the data
+--   type name is unambiguous, but not all of its constructors can be
+--   identified unambiguously.
+--   Returns multiple pairs of module names and constructor lists if the data
+--   type name is ambiguous.
 lookupConEntries
   :: TypeName a -> Environment a -> [(Maybe (S.ModuleName a), Maybe [ConEntry a])]
 lookupConEntries typeName env =
@@ -52,12 +57,16 @@ lookupConEntries typeName env =
       (lookupWith interfaceDataCons typeName env)
 
 -- | Looks up the data type names belonging to the the given data constructor
---   name in the given environment. The result includes the names of the
---   modules the types came from, if available.
+--   name in the given environment and qualifies them so that they are
+--   unambiguous, if possible. The result includes the names of the modules the
+--   types came from, where available.
 --
 --   Returns an empty list if the data constructor name is not in scope.
---   Returns multiple module and data type names if the data constructor name
---   is ambiguous.
+--   Returns a single pair with the second entry being @Nothing@ if the given
+--   data constructor name is unambiguous, but the data type it belongs to
+--   cannot be identified unambiguously.
+--   Returns multiple pairs of module and data type names if the data
+--   constructor name is ambiguous.
 lookupTypeName
   :: ConName a -> Environment a -> [(Maybe (S.ModuleName a), Maybe (TypeName a))]
 lookupTypeName conName env =
@@ -67,11 +76,12 @@ lookupTypeName conName env =
 -------------------------------------------------------------------------------
 -- Lookup Utility Functions                                                  --
 -------------------------------------------------------------------------------
--- TODO
--- | A generic version of 'lookupConEntries' and 'lookupTypeName' that
---   additionally takes a function mapping a module interface to the desired
---   map, i. e. one of the field names of 'ModuleInterface', as its first
---   argument.
+-- | Looks up the given data type or constructor name in the maps gotten when
+--   applying the given function to the module interfaces in the given
+--   environment. In addition to the lists of data constructor entries or data
+--   type names found, the result includes the import declarations, where
+--   available, and the module interfaces of the modules the found values came
+--   from.
 lookupWith :: (ModuleInterface a -> Map (S.QName a) v)
            -> S.QName a
            -> Environment a
@@ -79,59 +89,80 @@ lookupWith :: (ModuleInterface a -> Map (S.QName a) v)
 lookupWith getMap qName env = mapMaybe
   (\x -> fmap (x, ) (Map.lookup (unQualifyName qName) (getMap (snd x))))
   (possibleInterfaces qName env)
+ where
+  -- | Removes the possible qualification of the given 'S.QName'.
+  --
+  --   Other 'S.QName's, including special names for built-in data
+  --   constructors, are returned as given.
+  unQualifyName :: S.QName a -> S.QName a
+  unQualifyName (S.Qual s _ name) = S.UnQual s name
+  unQualifyName uqName            = uqName
 
--- TODO
--- | Returns the list of all module interfaces in the given environment the
---   given possibly qualified name could refer to.
+-- | Returns the list of all module interfaces in the given environment with
+--   their import declarations, where available, that the given possibly
+--   qualified name could refer to.
 possibleInterfaces
   :: S.QName a -> Environment a -> [(Maybe (S.ImportDecl a), ModuleInterface a)]
 possibleInterfaces qName env = filter (fitsToInterface qName . snd)
   [(Nothing, envCurrentModule env), (Nothing, envOtherEntries env)]
   ++ map (\(x, y) -> (Just x, y))
          (filter (fitsToImport qName . fst) (envImportedModules env))
+ where
+  -- | Checks if the given possibly qualified name could refer to an entry of
+  --   the given module interface.
+  --
+  --   It is assumed that the given module interface is imported unqualified
+  --   since it should either belong to the current module or implicitly
+  --   imported modules. It therefore returns @True@ for names qualified with
+  --   the module interface name and all unqualified names.
+  fitsToInterface :: S.QName a -> ModuleInterface a -> Bool
+  fitsToInterface (S.Qual _ modName _) = elem modName . interfaceModName
+  fitsToInterface _                    = const True
 
--- | Checks if the given possibly qualified name could refer to an entry of the
---   given module interface.
---
---   It is assumed that the given module interface is imported unqualified
---   since it should either belong to the current module or implicitly imported
---   modules. It therefore returns @True@ for names qualified with the module
---   interface name and all unqualified names.
-fitsToInterface :: S.QName a -> ModuleInterface a -> Bool
-fitsToInterface (S.Qual _ modName _) = elem modName . interfaceModName
-fitsToInterface _                    = const True
+  -- | Checks if the given possibly qualified name could refer to an entry of a
+  --   module imported with the given import declaration.
+  fitsToImport :: S.QName a -> S.ImportDecl a -> Bool
+  fitsToImport (S.Qual _ modName _) = (==) modName . getImportQualifier
+  fitsToImport _ = not . S.importIsQual
 
--- | Checks if the given possibly qualified name could refer to an entry of a
---   module imported with the given import declaration.
-fitsToImport :: S.QName a -> S.ImportDecl a -> Bool
-fitsToImport (S.Qual _ modName _) = (==) modName . getImportQualifier
-fitsToImport _ = not . S.importIsQual
-
+-- | Gets the qualifier that identifiers referring to a module imported by the
+--   given import declaration are possibly qualified by.
 getImportQualifier :: S.ImportDecl a -> S.ModuleName a
 getImportQualifier importDecl =
   fromMaybe (S.importModule importDecl) (S.importAsName importDecl)
 
--- | Removes the possible qualification of the given 'S.QName'.
+-------------------------------------------------------------------------------
+-- Qualification of Lookup Results                                           --
+-------------------------------------------------------------------------------
+-- | Uses the given qualification function (first argument) and the given
+--   qualification information (first element of the second argument) to
+--   qualify the given lookup result (second element of the second argument).
 --
---   Other 'S.QName's, including special names for built-in data constructors,
---   are returned as given.
-unQualifyName :: S.QName a -> S.QName a
-unQualifyName (S.Qual s _ name) = S.UnQual s name
-unQualifyName uqName            = uqName
-
+--   In addition to the qualified lookup result, the module name retrieved from
+--   the given qualification information is returned.
 qualifyLookupResult :: (Maybe (Bool, S.ModuleName a) -> b -> c)
                     -> ((Maybe (S.ImportDecl a), ModuleInterface a), b)
                     -> (Maybe (S.ModuleName a), c)
 qualifyLookupResult qualify (qualInfo@(_, interface), lookupResult) =
-  (interfaceModName interface, qualify (transformQualInfo qualInfo) lookupResult)
+  (interfaceModName interface, qualify (simplifyQualInfo qualInfo) lookupResult)
  where
-  transformQualInfo :: (Maybe (S.ImportDecl a), ModuleInterface a)
-                    -> Maybe (Bool, S.ModuleName a)
-  transformQualInfo (Just importDecl, _) =
+  -- | Simplifies qualification information consisting of a module interface
+  --   and possibly an import declaration in the following way:
+  --   If the qualification information does not specify a name, @Nothing@ is
+  --   returned.
+  --   Otherwise, a bool specifying whether identifiers must be qualified and
+  --   the module name that could be used as a qualifier are returned.
+  simplifyQualInfo :: (Maybe (S.ImportDecl a), ModuleInterface a)
+                   -> Maybe (Bool, S.ModuleName a)
+  simplifyQualInfo (Just importDecl, _) =
     Just (S.importIsQual importDecl, getImportQualifier importDecl)
-  transformQualInfo (Nothing, interface') =
+  simplifyQualInfo (Nothing, interface') =
     fmap (False, ) (interfaceModName interface')
 
+-- | Qualifies the given data constructor entry based on the given
+--   qualification information so that neither the data constructor name nor
+--   the data type name of the constructor entry are ambiguous in the given
+--   environment. Returns @Nothing@ if that is not possible.
 qualifyConEntry
   :: Environment a -> Maybe (Bool, S.ModuleName a) -> ConEntry a -> Maybe (ConEntry a)
 qualifyConEntry env qualInfo conEntry =
@@ -143,6 +174,10 @@ qualifyConEntry env qualInfo conEntry =
                                                    }
     _ -> Nothing
 
+-- | Qualifies the given possibly qualified name based on the given
+--   qualification information so that it is not ambiguous in its namespace of
+--   the given environment, where the namespace (data types or constructors) is
+--   specified by the given function. Returns @Nothing@ if that is not possible.
 qualifyQNameEnv :: (ModuleInterface a -> Map (S.QName a) v)
                 -> Environment a
                 -> Maybe (Bool, S.ModuleName a)
@@ -157,7 +192,11 @@ qualifyQNameEnv getMap env (Just (mustBeQual, modName)) uqName =
          in if length (lookupWith getMap qName env) == 1
               then Just qName
               else Nothing
-
-qualifyQName :: S.QName a -> S.ModuleName a -> S.QName a
-qualifyQName (S.UnQual s name) modName = S.Qual s modName name
-qualifyQName qName _ = qName
+ where
+  -- | Qualifies the given unqualified 'S.QName' by the given module name.
+  --
+  --   Already qualified names and special names for built-in data constructors
+  --   are returned as given.
+  qualifyQName :: S.QName a -> S.ModuleName a -> S.QName a
+  qualifyQName (S.UnQual s name) modName' = S.Qual s modName' name
+  qualifyQName qName _ = qName
--- src/lib/HST/Environment/LookupOrReport.hs
+++ src/lib/HST/Environment/LookupOrReport.hs
@@ -1,8 +1,10 @@
 -- | This module contains variations of the "HST.Environment" lookup functions
---   that remove the list and pair constructor wrapping the return type.
+--   that remove the list, pair and @Maybe@ type wrapping the desired return
+--   type.
 --
---   If the original function does not return a singleton list, a fatal error
---   mentioning the out-of-scope or ambiguous entry is reported.
+--   If the original lookup function does not return a singleton list
+--   containing @Just@ the desired return type, a fatal error mentioning the
+--   problem case is reported.
 module HST.Environment.LookupOrReport
   ( lookupConEntriesOrReport
   , lookupTypeNameOrReport
@@ -19,12 +21,15 @@ import qualified HST.Frontend.Syntax    as S
 import           HST.Util.Messages      ( Severity(Error), message )
 import           HST.Util.PrettyName    ( prettyName )
 
--- | Looks up the constructors belonging to the the given data type name in the
---   current environment.
+-- | Looks up the data constructor entries belonging to the the given data type
+--   name in the current environment and qualifies them so that they are
+--   unambiguous.
 --
 --   Reports a fatal error if the data type could not be found or if it could
 --   be found in multiple modules. In the latter case, the names of
---   these modules are displayed as part of the error message.
+--   these modules are displayed as part of the error message. A fatal error is
+--   also reported if not all constructors found could be identified
+--   unambiguously.
 lookupConEntriesOrReport
   :: Members '[Env a, Report] r => TypeName a -> Sem r [ConEntry a]
 lookupConEntriesOrReport typeName = do
@@ -47,12 +52,14 @@ lookupConEntriesOrReport typeName = do
       ++ "` could refer to one of the following modules: "
       ++ displayModuleNames conEntriess
 
--- | Looks up the data type names belonging to the the given data constructor
---   name in the current environment.
+-- | Looks up the data type name belonging to the the given data constructor
+--   name in the current environment and qualifies it so that it is
+--   unambiguous.
 --
 --   Reports a fatal error if the data constructor could not be found or if it
 --   could be found in multiple modules. In the latter case, the names of these
---   modules are displayed as part of the error message.
+--   modules are displayed as part of the error message. A fatal error is also
+--   reported if the data type found could not be identified unambiguously.
 lookupTypeNameOrReport
   :: Members '[Env a, Report] r => ConName a -> Sem r (TypeName a)
 lookupTypeNameOrReport conName = do
@@ -75,7 +82,7 @@ lookupTypeNameOrReport conName = do
       ++ "` could refer to one of the following modules: "
       ++ displayModuleNames typeNames
 
--- | Display the module names contained in the first pair entries of the given
+-- | Displays the module names contained in the first pair entries of the given
 --   list.
 --
 --   As no modules without a module name should be able to be imported
